34678ec4342662f44c1eab02fd71d8b53fe95492
Олена Пестова <nowalena9@gmail.com>
2023-03-19 02:28:36 +0200

    wrong work 404 and name.official doesnt get

--- src/country-box.js
+++ src/country-box.js
@@ -1,28 +1,46 @@
 import fetchCountries from "./fetchCountries";
 import DEBOUNCE_DELAY from "./index";
+import Notiflix from "notiflix";
 
 const refs = {
     input: document.querySelector('#search-box'),
     list: document.querySelector('.country-list'),
     info: document.querySelector('.country-info'),
 };
-
-refs.input.addEventListener('input', _.debounce(onSearch, DEBOUNCE_DELAY));
+ const debouncedInput = _.debounce(onSearch, DEBOUNCE_DELAY,{leading:true,trailing:false});
+refs.input.addEventListener('input',debouncedInput);
 
 function onSearch(e) {
-    const searchCountry = e.currentTarget.value;
+   
+  const searchCountry = e.currentTarget.value;
+  if (!searchCountry) {
+    refs.list.innerHTML = "";
+    refs.info.innerHTML = "";
+    return;
+   }
     fetchCountries(searchCountry)
-        .then((country) => {
-            console.log(country);  //official, capital, population, flags.svg, languages
-        // render();
+      .then((countries) => {
+          console.log(countries);
+          if (countries) {
+            let markup = '';
+            if (countries.length > 1) {
+               markup = countries.map(({ name: { official }, flags: { svg }}) => `<li><svg width="100" height="100">${flags}</svg>${name}</li>`);
+             
+              refs.list.innerHTML = markup;
+            }
+            else {
+              const { name: { official }, flags: { svg }, capital, population, languages } = countries[0];
+              const language = Object.values(languages).join(", ");
+              markup = `<svg width="100" height="100">${flags}</svg><p>${name}</p><p>${capital}</p><p>${population}</p><p>${language}</p>`;
+              refs.list.innerHTML = "";
+              refs.info.innerHTML = markup;
+            }
+          }
       })
       .catch((error) => {
         console.log(error);
       })
-    //   .finally(() => {
-    //     e.target.reset();
-    //   });
-    
+  
 };
 function render() {
     
--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,28 +1,38 @@
-//export  fetchCountries ;
-const BASE_URL = 'https://restcountries.com/v3.1/name/';
-// const API_KEY = '';
-// const OPTIONS = {
-//   headers: {
-//     Authorization: API_KEY,
-//   },
-// };
-//  let page = 1;
-  
- export function  fetchCountries(name) {
-      return fetch(`${BASE_URL}${name}`, OPTIONS) //https://restcountries.com/v3.1/name/{name}
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(response.status);
-        }
-        return response.json();
+import Notiflix from "notiflix";
+
+export default fetchCountries ;
+const BASE_URL = 'https://restcountries.com/v3.1/name/'; //https://restcountries.com/v3.1/name/deutschland
+const API_KEY = "";
+
+const OPTIONS = {
+  headers: {
+   // Authorization: API_KEY,
+  },
+};
+//https://restcountries.com/v3.1/name?fields=name,capital,currencies  
+//https://restcountries.com/v3.1/sweden?fields=name.official,capital,population,flags.svg,languages
+const searchParams = '?fields=name.official,capital,population,flags.svg,languages';
+function fetchCountries(name) {
+ 
+  name = name.trim();
+  console.log(`${BASE_URL}${name}${searchParams}`);
+  return fetch(`${BASE_URL}${name}${searchParams}`, OPTIONS) 
+    .then(response => {
+         if (!response.ok) {
+          Notiflix.Notify.failure("Oops, there is no country with that name");
+            }
+        else return response.json();
       })
-      .then(({country}) => {country})
+    .then((data) => {
+      console.log("data.length ",data.length);
+      if (data.length > 10) {
+        Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+        return;
+      }
+       return data;
+    })
       .catch(error => {
         console.error(error);
       });
   }
-// name.official - повна назва країни
-// capital - столиця
-// population - населення
-// flags.svg - посилання на зображення прапора
-// languages 
+
